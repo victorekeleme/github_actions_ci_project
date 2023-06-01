529f5b14fabde29d0eeb183a314444888de4a36f
victorekeleme <ekelemevictor99@gmail.com>
2023-05-31 20:26:52 -0400

    updated

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,11 +10,13 @@ import org.slf4j.LoggerFactory;
 public class Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);}
+        SpringApplication.run(Application.class, args);
+    }
 
     public void init(){
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started");}
+        log.info("Java app started");
+    }
 
     public String getStatus() {
         return "OK";
