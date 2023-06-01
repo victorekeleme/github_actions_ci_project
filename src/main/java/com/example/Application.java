ecb75f56cdc6ac5396b19c651c3e2c8fff28ef80
victorekeleme <ekelemevictor99@gmail.com>
2023-05-31 20:25:25 -0400

    updated

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,22 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);}
 
-    @PostConstruct
-    public void init()
-    {
+    public void init(){
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started");
-    }
+        log.info("Java app started");}
 
     public String getStatus() {
         return "OK";
